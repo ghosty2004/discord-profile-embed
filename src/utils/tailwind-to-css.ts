66bb5f3cf3b0377f25ca17a9b489cfbdcfce1cac
ghosty2004 <me@ghosty2004.dev>
2024-07-14 00:02:39 +0300

    feat: add cache for twj evaluation

--- src/utils/tailwind-to-css.ts
+++ src/utils/tailwind-to-css.ts
@@ -1,10 +1,24 @@
 import { tailwindToCSS } from 'tw-to-css';
 import config from '../../tailwind.config';
+import { ExpectedAny, ExpectedStr } from '../types';
 
-const { twi, twj } = tailwindToCSS({
+const { twi: TWI, twj: TWJ } = tailwindToCSS({
   config: {
     theme: config.theme,
   },
 });
 
-export { twi, twj };
+export const twi = TWI;
+
+const cacheTwj = new Map<ExpectedStr, ExpectedAny>();
+
+export const twj = (input: ExpectedStr) => {
+  if (cacheTwj.has(input)) {
+    return cacheTwj.get(input);
+  }
+
+  const result = TWJ(input, { minify: true });
+  cacheTwj.set(input, result);
+
+  return result;
+};
