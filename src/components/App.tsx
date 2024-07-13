081d3556f0e233d5aa6fad6bf8770e50d3f4a9ff
ghosty2004 <me@ghosty2004.dev>
2024-07-14 00:03:54 +0300

    refactor: style with twj call instead of className

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { MdDoNotDisturbOn } from 'react-icons/md';
 import { FaMoon } from 'react-icons/fa';
 import { GoDotFill } from 'react-icons/go';
+import { twj } from '../utils/tailwind-to-css';
 import { Spotify, SpotifyActivity } from './activities/Spotify';
 import { TBioNode } from '../types';
 
@@ -20,7 +21,7 @@ interface IProps {
 }
 
 const DEFAULT_WIDTH = 310;
-const DEFAULT_HEIGHT = 510;
+const DEFAULT_HEIGHT = 520;
 
 const App = ({
   username,
@@ -44,7 +45,6 @@ const App = ({
       width={`${DEFAULT_WIDTH}px`}
       height={`${height}px`}
     >
-      {/* <style dangerouslySetInnerHTML={{ __html: '.red{color:red}' }} /> */}
       <foreignObject
         x="0"
         y="0"
@@ -54,93 +54,113 @@ const App = ({
         <div
           // @ts-ignore
           xmlns="http://www.w3.org/1999/xhtml"
-          className={`absolute w-[${DEFAULT_WIDTH - 10}px] h-[${
-            height - 10
-          }px] inset-0 bg-[#1a1c1f] font-default text-white text-[16px] flex flex-col rounded-[10px] gap-10`}
+          style={twj(
+            `absolute w-[${DEFAULT_WIDTH - 10}px] h-[${
+              height - 10
+            }px] inset-0 bg-[#1a1c1f] font-default text-white text-[16px] flex flex-col rounded-[10px] gap-10`,
+          )}
         >
-          <div className="h-[100px] relative flex flex-col gap-10">
+          <div style={twj('h-[100px] relative flex flex-col gap-10')}>
             {bannerDataUri ? (
               <img
                 src={bannerDataUri}
                 alt="banner"
-                className="w-full h-full object-fill rounded-t-[10px]"
+                style={twj('w-full h-full object-fill rounded-t-[10px]')}
               />
             ) : (
-              <div className="w-full h-full bg-gray-500 rounded-t-[10px]" />
+              <div style={twj('w-full h-full bg-gray-500 rounded-t-[10px]')} />
             )}
 
-            <div className="absolute top-[80%] left-[2%] bottom-0 rounded-t-[10px] flex items-center justify-center">
-              <div className="relative w-[80px] h-[80px]">
+            <div
+              style={twj(
+                'absolute top-[80%] left-[2%] bottom-0 rounded-t-[10px] flex items-center justify-center',
+              )}
+            >
+              <div style={twj('relative w-[80px] h-[80px]')}>
                 <img
                   src={avatarDataUri}
                   alt="avatar"
-                  className={`rounded-full w-[76px] h-[76px] border-[2px] ${
-                    !avatarDecorationDataUri ? 'border-solid border-white' : ''
-                  }`}
+                  style={twj(
+                    `rounded-full w-[76px] h-[76px] border-[2px] ${
+                      !avatarDecorationDataUri
+                        ? 'border-solid border-white'
+                        : ''
+                    }`,
+                  )}
                 />
                 {avatarDecorationDataUri && (
                   <img
                     src={avatarDecorationDataUri}
                     alt="avatar-decoration"
-                    className="absolute inset-0 w-full h-full"
+                    style={twj('absolute inset-0 w-full h-full')}
                   />
                 )}
-                <div className="absolute bottom-[2px] right-[10px]">
+                <div style={twj('absolute bottom-[2px] right-[10px]')}>
                   {status === 'dnd' ? (
                     <MdDoNotDisturbOn
                       width={10}
                       height={10}
-                      className="text-red-500 bg-[#1a1c1f] p-[2px] rounded-full"
+                      style={twj(
+                        'text-red-500 bg-[#1a1c1f] p-[2px] rounded-full',
+                      )}
                     />
                   ) : status === 'online' ? (
-                    <div className="w-[10px] h-[10px] bg-green-500 rounded-full" />
+                    <div
+                      style={twj('w-[10px] h-[10px] bg-green-500 rounded-full')}
+                    />
                   ) : status === 'idle' ? (
                     <FaMoon
                       width={10}
                       height={10}
-                      className="text-yellow-500 bg-[#1a1c1f] p-[2px] rounded-full"
+                      style={twj(
+                        'text-yellow-500 bg-[#1a1c1f] p-[2px] rounded-full',
+                      )}
                     />
                   ) : (
-                    <div className="w-[10px] h-[10px] bg-gray-500 rounded-full" />
+                    <div
+                      style={twj('w-[10px] h-[10px] bg-gray-500 rounded-full')}
+                    />
                   )}
                 </div>
               </div>
             </div>
 
-            <div className="absolute top-[105%] right-[2%] bottom-0">
-              <div className="flex flex-row gap-2 bg-[#121315] rounded-md p-1">
+            <div style={twj('absolute top-[105%] right-[2%] bottom-0')}>
+              <div
+                style={twj('flex flex-row gap-2 bg-[#121315] rounded-md p-1')}
+              >
                 {badgeDataUris.map((badge) => (
                   <img
                     key={badge}
                     src={badge}
                     alt="badge"
-                    className="w-[20px] h-[20px] rounded-md"
+                    style={twj('w-[20px] h-[20px] rounded-md')}
                   />
                 ))}
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col mx-2 gap-5">
-            <div className="flex flex-col gap-1 rounded-md p-2 bg-[#121315]">
-              <p className="text-xl font-bold m-0 p-0">{globalName}</p>
-              <div className="flex items-center gap-1">
-                <p className="text-sm m-0 p-0">{username}</p>
-                <GoDotFill className="text-xs" />
-                <p className="text-sm text-white/80 m-0 p-0">{pronouns}</p>
+          <div style={twj('flex flex-col mx-2 gap-5')}>
+            <div style={twj('flex flex-col gap-1 rounded-md p-2 bg-[#121315]')}>
+              <p style={twj('text-xl font-bold m-0 p-0')}>{globalName}</p>
+              <div style={twj('flex items-center gap-1')}>
+                <p style={twj('text-sm m-0 p-0')}>{username}</p>
+                <GoDotFill style={twj('text-xs')} />
+                <p style={twj('text-sm text-white/80 m-0 p-0')}>{pronouns}</p>
               </div>
             </div>
 
-            <div className="flex flex-col rounded-md p-2 gap-2 bg-[#121315]">
-              <p className="p-0 m-0 text-sm text-white/90 font-semibold">
+            <div style={twj('flex flex-col rounded-md p-2 gap-2 bg-[#121315]')}>
+              <p style={twj('p-0 m-0 text-sm text-white/90 font-semibold')}>
                 About Me
               </p>
 
               {bio.map((line, index) => (
-                <div key={index} className="flex flex-row">
+                <div key={index} style={twj('flex flex-row')}>
                   {line.map((node, index) =>
                     node.type === 'char' ? (
-                      <p key={index} className="m-0 p-0">
+                      <p key={index} style={twj('m-0 p-0')}>
                         {node.value}
                       </p>
                     ) : (
@@ -148,7 +168,7 @@ const App = ({
                         key={index}
                         src={node.value}
                         alt="emoji"
-                        className="w-[20px] h-[20px]"
+                        style={twj('w-[20px] h-[20px]')}
                       />
                     ),
                   )}
@@ -157,7 +177,7 @@ const App = ({
             </div>
 
             {currentActivity ? (
-              <div className="rounded-md p-2 bg-[#121315]">
+              <div style={twj('rounded-md p-2 bg-[#121315]')}>
                 {currentActivity instanceof SpotifyActivity ? (
                   <Spotify {...currentActivity} />
                 ) : null}
--- src/components/activities/Spotify.tsx
+++ src/components/activities/Spotify.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaSpotify } from 'react-icons/fa';
+import { twj } from '../../utils/tailwind-to-css';
 
 export class SpotifyActivity {
   albumDataUri: string;
@@ -27,35 +28,41 @@ export const Spotify = ({
   totalDuration,
   progress,
 }: Required<SpotifyActivity>) => (
-  <div className="flex flex-col gap-2">
-    <p className="flex items-center gap-1 p-0 m-0 text-sm text-white/90 font-semibold">
+  <div style={twj('flex flex-col gap-2')}>
+    <p
+      style={twj(
+        'flex items-center gap-1 p-0 m-0 text-sm text-white/90 font-semibold',
+      )}
+    >
       Listening to Spotify
-      <FaSpotify className="text-green-500" />
+      <FaSpotify style={twj('text-green-500')} />
     </p>
 
-    <div className="flex">
+    <div style={twj('flex')}>
       <img
         src={albumDataUri}
         alt="album"
-        className="w-[50px] h-[50px] rounded-md"
+        style={twj('w-[50px] h-[50px] rounded-md')}
       />
 
-      <div className="flex flex-col gap-1 ml-2">
-        <p className="text-sm text-white/90 m-0 p-0">{songName}</p>
-        <p className="text-xs text-white/70 m-0 p-0">{artistName}</p>
+      <div style={twj('flex flex-col gap-1 ml-2')}>
+        <p style={twj('text-sm text-white/90 m-0 p-0')}>{songName}</p>
+        <p style={twj('text-xs text-white/70 m-0 p-0')}>{artistName}</p>
       </div>
     </div>
 
-    <div className="flex flex-col gap-2">
-      <div className="w-full relative h-[3px] bg-gray-300 rounded-lg">
+    <div style={twj('flex flex-col gap-2')}>
+      <div style={twj('w-full relative h-[3px] bg-gray-300 rounded-lg')}>
         <div
-          className={`absolute top-0 left-0 h-full bg-blue-500 w-[${progress}%]`}
+          style={twj(
+            `absolute top-0 left-0 h-full bg-blue-500 w-[${progress}%]`,
+          )}
         ></div>
       </div>
 
-      <div className="flex justify-between">
-        <p className="m-0 p-0 text-xs text-white/70">{elapsed}</p>
-        <p className="m-0 p-0 text-xs text-white/70">{totalDuration}</p>
+      <div style={twj('flex justify-between')}>
+        <p style={twj('m-0 p-0 text-xs text-white/70')}>{elapsed}</p>
+        <p style={twj('m-0 p-0 text-xs text-white/70')}>{totalDuration}</p>
       </div>
     </div>
   </div>
